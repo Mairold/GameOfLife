013eae71567aa7ad0a06e5019c7a9309cce748a7
janelikurm <janelikurm@gmail.com>
2023-05-04 11:15:55 +0300

    Gameboard Background Colors added.

--- src/Game.svelte
+++ src/Game.svelte
@@ -1,40 +1,31 @@
 <script>
 
- import {onMount} from "svelte";
+    import {onMount} from "svelte";
 
- let gameGrid = []
- let height
- let width
- let gameBoard;
+    let gameGrid = []
+    let gameBoard
 
- function calculateGameBoard() {
-     let rect = gameBoard.getBoundingClientRect();
-     console.log('height' + height)
-     console.log('height offset ' + rect.height)
-     console.log('width ' + width)
-     console.log('width offset ' + gameBoard.offsetWidth)
-     let dividedWidth = Math.floor(gameBoard.offsetWidth / 20);
-     let dividedHeight = Math.floor(gameBoard.offsetHeight / 20);
-     console.log('divHeight ' + dividedHeight)
-     console.log('divWidth ' + dividedWidth)
-     let arrayLength = (dividedWidth * dividedHeight)
-     console.log(arrayLength)
-     gameGrid = Array(arrayLength)
+    function calculateGameBoard() {
+        let rows = Math.floor(gameBoard.offsetWidth / 20)
+        let columns = Math.floor(gameBoard.offsetHeight / 20)
+        gameGrid = Array.from({length: (rows * columns)}, (v, i) => i)
+    }
 
-     console.log(gameGrid.length)
+    onresize = calculateGameBoard
+    onMount(() => calculateGameBoard())
 
- }
- onresize = calculateGameBoard
- onMount(() => calculateGameBoard())
+    function setCellAlive(e) {
+        document.getElementById(e.target.id).classList.toggle('cheese')
+        // document.getElementById(e.target.id).classList.toggle('cell')
+    }
 
 </script>
 <div class="div">
-    <div bind:clientWidth={width} bind:clientHeight={height} bind:this={gameBoard} class="gameBoard">
-        {#each gameGrid as e}
-            <div class="box"></div>
+    <div bind:this={gameBoard} class="gameBoard">
+        {#each gameGrid as cell}
+            <div id="{'cell' + cell}" on:click={setCellAlive} class="cell cheese"></div>
         {/each}
     </div>
-
 </div>
 
 <style>
@@ -48,13 +39,18 @@
 
     .gameBoard {
         display: grid;
-        grid-template: repeat(auto-fill,  20px) / repeat(auto-fill,  20px);
+        grid-template: repeat(auto-fill, 20px) / repeat(auto-fill, 20px);
         width: 90%;
         height: 100%;
     }
 
-    .box {
-        border: rgb(183, 183, 187) solid 0.01rem  ;
-        background-color: #949191;
+    .cell {
+        border: #FFF8D6 solid 0.01rem;
+        background-color: #A4D0A4;
+    }
+
+    .cheese {
+        background-color: #F7E1AE;
+        z-index: 1;
     }
 </style>
