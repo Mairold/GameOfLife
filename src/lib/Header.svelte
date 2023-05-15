6e5290a4b21573f738b6a49a4c7d1ed4053b49a3
janelikurm <janelikurm@gmail.com>
2023-05-15 15:10:15 +0300

    header picture added

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,9 +1,20 @@
 <script>
 
 </script>
-<div>
-<h1>Game of Life</h1>
-</div>
-<style>
 
+<div class="header">
+    <h1>Game of Life</h1>
+    <img class="titleImage" src="src/assets/MMH3_Logo_RoE.jpg">
+</div>
+
+<style>
+    .titleImage {
+        width: 200px;
+        height: 80px;
+    }
+
+    .header {
+        display: flex;
+        flex-flow: row nowrap;
+    }
 </style>
